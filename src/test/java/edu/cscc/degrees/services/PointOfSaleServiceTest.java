0775f1ca3cd475000f93ac72442755520a9fec72
Jeff Anderson <janderson141@cscc.edu>
2021-05-05 11:45:25 -0400

    Add tests root to starting point

---
+++ src/test/java/edu/cscc/degrees/services/PointOfSaleServiceTest.java
@@ -0,0 +1,20 @@
+package edu.cscc.degrees.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class PointOfSaleServiceTest {
+
+  @Test
+  @DisplayName("It runs tests")
+  public void test00() {
+    assertTrue(true);
+  }
+
+
+}
